3cec94e15b6039203192251a51f6eec21bd65f5c
Erwann.Gallois <gallois.erwann@gmail.com>
2022-01-26 14:20:24 +0100

    Validé

--- Demarche scientifique/analyse.py
+++ Demarche scientifique/analyse.py
@@ -1,30 +1,28 @@
 file = open("sequence/seq_TD1.txt", "r")
-file_write = open("sortie/seq_TD1_sortie.txt", "a")
+file_write = open("sortie/seq_TD1_sortie.txt", "w")
 seq = file.readlines()
 adn = True
+taille = int(input("Entrer la taille de la fenetre"))
 ligne = 0
-n = "\n"
-n  = str(n)
-while 'n' in seq:
-    seq.remove(n)
-print(seq)
 for line in seq: 
     ligne = ligne + 1
 chaine = ''.join([str(elem)for elem in seq[1:ligne+1]])
 for i in chaine:
-    if i not in "atgc\n":
+    if i not in "atgcn\n":
         adn = False
         break
 if (adn == True):
     print("Valide")
-    for i in (range(1,11)):
-        tg = chaine[i:i+10].count("g")
-        tc = chaine[i:i+10].count("c")
-        ta = chaine[i:i+10].count("a")
-        tt = chaine[i:i+10].count("t")
-        total = tg + tc + ta + tt
-    tauxgc = ((tg + tc) / total) *100
-    print(total)
-    print(tauxgc)
+    for i in (range(1,len(chaine),taille)):
+        tg = chaine[i:i+taille].count("g")
+        tc = chaine[i:i+taille].count("c")
+        ta = chaine[i:i+taille].count("a")
+        tt = chaine[i:i+taille].count("t")
+        tauxgc = ((tg + tc) /taille) *100
+        stri = str(i)
+        strtauxgc = str(tauxgc)
+        ecrire = stri+"\t"+strtauxgc+"\n"
+        file_write.write(ecrire)
+    print("Fichier creer")
 else:
     print("Invalide")
