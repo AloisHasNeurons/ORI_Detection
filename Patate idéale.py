69d2ac9dbea7c92f3939c4560de3d45640c4c061
Erwann.Gallois <gallois.erwann@gmail.com>
2022-02-10 09:02:42 +0100

    Réorganisation de Patate idéale

--- Patate idéale.py
+++ Patate idéale.py
@@ -15,13 +15,11 @@ seq = file.readlines()
 # Récupération du nombre de lignes
 for line in seq:
     ligne = ligne + 1
-#
+
 chaine = ''.join([str(elem)for elem in seq[1:ligne+1]])
 chaine = supprrc(chaine)
 longueur = len(chaine)
 file.close()
-
-file_write = open("tauxGC.txt", "w")
 
 # Vérification de la séquence
 adn = validation(chaine)
@@ -32,13 +30,14 @@ adn = validation(chaine)
 # On peut calculer le taux de GC uniquement si la séquence est valide
 if (adn == True):
     print("Séquence valide")
-
     tailleF, pas = inputfp()
+    file_write = open("tauxGC.txt", "w")
     for i in (range(0, longueur+1, pas)):
-        # Calcul du taux de GC pour i
+        # Calcul du taux de GC dans la taille de la fenêtre
         tauxgcV = tauxgc(i, chaine, tailleF)
         ecriture(position, tauxgcV, file_write)
         position = checkposition(position, chaine, pas)
     print("Fichier de taux de GC créé")
+    file_write.close()
 else:
     print("Séquence invalide")
