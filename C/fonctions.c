da930a77f06e2b381a53f4c556fcd2e29fe3a567
Erwann.Gallois <gallois.erwann@gmail.com>
2022-04-11 10:25:47 +0200

    Commencement programme en c

--- C/fonctions.c
+++ C/fonctions.c
@@ -1,4 +1,50 @@
-char tauxgc(int i, int chaine, int taille)
+int cpt_lignes(FILE *fichier)
 {
-    int tg = 0;
+    int c;
+    int nLignes = 0;
+    int c2 = '\0';
+    while ((c = fgetc(fichier)) != EOF)
+    {
+        if (c == '\n')
+            nLignes++;
+        c2 = c;
+    }
+    /* Ici, c2 est égal au caractère juste avant le EOF. */
+    if (c2 != '\n')
+    {
+        nLignes++;
+    }
+    return nLignes;
+}
+int cpt_caracteres(FILE *fichier)
+{
+    int nCaracteres = 0;
+    while (fgetc(fichier) != EOF)
+    {
+        nCaracteres++;
+    }
+    return nCaracteres;
+}
+int cpt_caracteres_first_ligne ( FILE *fichier )
+{
+    int nCaracteres = 0;
+    int c;
+    while ((c = fgetc(fichier)) != '\n')
+    {
+        nCaracteres++;
+    }
+    return nCaracteres;
+}
+char* supp_retourchariot(char* chaine)
+{
+    int i = 0;
+    strlen(chaine);
+    for (i = 0; i < strlen(chaine); i++)
+    {
+        if (chaine[i] == '\n')
+        {
+            chaine[i] = '\0';
+        }
+    }
+    return chaine;
 }
--- C/patate_ideale.c
+++ C/patate_ideale.c
@@ -6,28 +6,36 @@
 #include <time.h>
 #include "fonctions.c"
 
-
-//open the file "Borrelia_burgdorferi_B31_complete_genome.txt" and read the data into a string called "data"
-char open_file(char *filename, char *data)
-{
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        printf("Error opening file!\n");
-        return 1;
-    }
-    fseek(file, 0, SEEK_END);
-    long fsize = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    fread(data, 1, fsize, file);
-    fclose(file);
-    return 0;
-}
-
 int main()
 {
-    char data[100000];
-    open_file("Borrelia_burgdorferi_B31_complete_genome.txt", data);
-    printf("%s", data);
+    FILE *fichier = NULL;
+    FILE* test = NULL;
+    char *sequence;
+    char* new_sequence;
+    int caracteres_first_ligne;
+    int caracteres;
+    int nb_lignes;
+    fichier = fopen("C:\\Users\\madgu\\OneDrive\\Cours-L1-Evry\\Bloc Complementaire\\Demarche_scientifique_projet\\La-patate-ideale\\La-patate-ideale\\Sequence\\Borrelia_burgdorferi_B31_complete_genome.txt", "r");
+    test = fopen("C:\\Users\\madgu\\OneDrive\\Cours-L1-Evry\\Bloc Complementaire\\Demarche_scientifique_projet\\La-patate-ideale\\La-patate-ideale\\Sequence\\test.txt", "w");
+    if (fichier != NULL)
+    {
+        nb_lignes = cpt_lignes(fichier);
+        printf("%d\n", nb_lignes);
+        rewind(fichier);
+        caracteres = cpt_caracteres(fichier);
+        printf("%d\n", caracteres);
+        rewind(fichier);
+        caracteres_first_ligne = cpt_caracteres_first_ligne(fichier);
+        fseek(fichier, caracteres_first_ligne+2, SEEK_SET);
+        sequence = malloc(caracteres-caracteres_first_ligne * sizeof(char));
+        fread(sequence, sizeof(char), caracteres-caracteres_first_ligne, fichier);
+        fputs(sequence, test);
+        fclose(fichier);
+        fclose(test);
+    }
+    else
+    {
+        printf("Erreur d'ouverture du fichier");
+    }
     return 0;
 }
