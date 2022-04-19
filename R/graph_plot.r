d72eb26d5f4b4795e0bca9bfb8481fd88f7ed961
CartonBrutal <unchatonbrutal@gmail.com>
2022-04-19 18:15:31 +0200

    Chemins d'accès linux

--- R/graph_plot.r
+++ R/graph_plot.r
@@ -6,7 +6,7 @@ os <- Sys.info()["sysname"]
 if (os == "Windows") {
     setwd("C:\\Users\\madgu\\OneDrive\\Cours-L1-Evry\\Bloc Complementaire\\Demarche_scientifique_projet\\Demarche-Scientifique-ORI")#nolint
     } else {
-    setwd("/home/alois/Bureau/Demarche_Scientifique/La-patate-ideale")
+    setwd("/home/alois/Bureau/Demarche-Scientifique-ORI")
 }
 # Load data
 data <- read.table("sortie/tauxGC.txt", sep = "\t", header = TRUE) # nolint
@@ -14,7 +14,7 @@ data <- read.table("sortie/tauxGC.txt", sep = "\t", header = TRUE) # nolint
 if (os == "Windows") {
    setwd("C:\\Users\\madgu\\OneDrive\\Cours-L1-Evry\\Bloc Complementaire\\Demarche_scientifique_projet\\Demarche-Scientifique-ORI\\sortie")#nolint
    } else {
-       setwd("/home/alois/Bureau/Demarche_Scientifique/La-patate-ideale/sortie")
+       setwd("/home/alois/Bureau/Demarche-Scientifique-ORI/sortie")
 }
 
 plot(data[,1], data[,2], type = "l", xlab = "Position en kilobases", ylab = "Taux GC", main = "ORI", ylim = c(20,50)) #nolint
@@ -27,7 +27,7 @@ os <- Sys.info()["sysname"]
 if (os == "Windows") {
     setwd("C:\\Users\\madgu\\OneDrive\\Cours-L1-Evry\\Bloc Complementaire\\Demarche_scientifique_projet\\Demarche-Scientifique-ORI")#nolint
     } else {
-    setwd("/home/alois/Bureau/Demarche_Scientifique/La-patate-ideale")
+    setwd("/home/alois/Bureau/Demarche-Scientifique-ORI")
 }
 # Load data
 data <- read.table("sortie/calcGC.txt", sep = "\t", header = TRUE) # nolint
@@ -35,7 +35,7 @@ data <- read.table("sortie/calcGC.txt", sep = "\t", header = TRUE) # nolint
 if (os == "Windows") {
    setwd("C:\\Users\\madgu\\OneDrive\\Cours-L1-Evry\\Bloc Complementaire\\Demarche_scientifique_projet\\Demarche-Scientifique-ORI\\sortie")#nolint
    } else {
-       setwd("/home/alois/Bureau/Demarche_Scientifique/La-patate-ideale/sortie")
+       setwd("/home/alois/Bureau/Demarche-Scientifique-ORI/sortie")
 }
 
 plot(data[,1], data[,2], type = "l", xlab = "Position en kilobases", ylab = "G-C/G+C", main = "ORI", ylim = c(-40,40)) #nolint
