cbae37dc1900963af0bff9db902d08179693b61b
Erwann.Gallois <gallois.erwann@gmail.com>
2022-04-19 16:16:34 +0200

    Changement couleur ligne en bleu

--- R/graph_plot.r
+++ R/graph_plot.r
@@ -39,5 +39,5 @@ if (os == "Windows") {
 }
 
 plot(data[,1], data[,2], type = "l", xlab = "Position en kilobases", ylab = "G-C/G+C", main = "ORI", ylim = c(-40,40)) #nolint
-abline(h = 0, col = "black", lwd = 2)
+abline(h = 0, col = "blue", lwd = 2)
 print("Second plot done")
