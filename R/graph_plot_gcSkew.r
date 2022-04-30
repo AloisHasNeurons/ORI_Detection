b726cae393658f0504b71b775c2d8a3d0eb05186
CartonBrutal <78328371+CartonBrutal@users.noreply.github.com>
2022-04-30 16:50:00 +0200

    Changements graphiques

--- R/graph_plot_gcSkew.r
+++ R/graph_plot_gcSkew.r
@@ -9,6 +9,6 @@ setwd("sortie")
 data <- read.table("gcSkew.txt", sep = "\t", header = TRUE) # nolint
 
 # Plot
-plot(data[, 1], data[, 2], type = "l", xlab = "Position en kilobases", ylab = "GC Skew", main = "ORI", ylim = c(-40, 40)) # nolint
+plot(data[, 1], data[, 2], type = "l", xlab = "Position en kilobases", ylab = "GC Skew", main = "Graphique representant le GC Skew en fonction des kilobases", ylim = c(-40, 40)) # nolint
 abline(h = 0, col = "blue", lwd = 2)
 print("Plot done")
--- R/graph_plot_tauxGC.r
+++ R/graph_plot_tauxGC.r
@@ -9,5 +9,5 @@ data <- read.table("tauxGC.txt", sep = "\t", header = TRUE) # nolint
 
 
 # Plot
-plot(data[, 1], data[, 2], type = "l", xlab = "Position en kilobases", ylab = "Taux GC", main = "ORI", ylim = c(20, 50)) # nolint
+plot(data[, 1], data[, 2], type = "l", xlab = "Position en kilobases", ylab = "Taux GC", main = "Graphique representant le taux de GC en fonction des kilobases", ylim = c(20, 50)) # nolint
 print("Plot done")
